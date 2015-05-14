735e5ea6e7e7315055fa6f1e60eb793150783fc9
Thierry LAU <thierlau@gmail.com>
2015-05-14 05:20:23 +0200

    MaterializeSelect

--- app/components/BrocNote.jsx
+++ app/components/BrocNote.jsx
@@ -1,6 +1,30 @@
+var MaterializeSelect = require('components/MaterializeSelect');
+
 var BrocNote = React.createClass({
+
+	getInitialState: function () {
+		return {
+			categories : [{
+				value: "vetements",
+				label: "Vêtements"
+			}, {
+				value: "livres",
+				label: "Livres"
+			}, {
+				value: "jouets",
+				label: "Jouets"
+			}]
+		}
+	},
+
 	render: function () {
-		return (<div>BrocNote</div>)
+		return (
+			<div className="container">
+				<div className="row">
+					<MaterializeSelect className="col s12" options={this.state.categories}></MaterializeSelect>
+				</div>
+			</div>
+		)
 	}
 });
 
---
+++ app/components/MaterializeSelect.jsx
@@ -0,0 +1,31 @@
+var MaterializeSelect = React.createClass({
+
+	propsType: {
+		options: React.PropTypes.arrayOf(React.PropTypes.shape({
+			value: React.PropTypes.string,
+			label: React.PropTypes.string
+		}))
+	},
+
+	componentDidMount: function () {
+		$(React.findDOMNode(this.refs.categories)).material_select();
+	},
+
+	render: function () {
+		var options = this.props.options.map(function (option) {
+			return <option value={option.value}>{option.label}</option>
+		});
+
+		return (
+			<div className={(this.props.className || '') + " input-field"}>
+				<select ref="categories">
+					<option defaultValue="0" disabled>Choisir une catégorie</option>
+					{options}
+				</select>
+				<label>Objets</label>
+			</div>
+		)
+	}
+});
+
+module.exports = MaterializeSelect;
