e1db828c3d34c98a8ba4f54a4707ae2fd4eb551b
Thierry LAU <thierlau@gmail.com>
2015-05-14 06:05:44 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -202,8 +202,8 @@ var BrocNote = React.createClass({displayName: 'BrocNote',
 			React.createElement("div", {className: "container"}, 
 				React.createElement("div", {className: "row"}, 
 					React.createElement(MaterializeSelect, {className: "col s12", options: this.state.categories}), 
-					React.createElement(MaterializeInput, {className: "col s6", label: "Nombre", type: "tel"}), 
-					React.createElement(MaterializeInput, {className: "col s6", label: "Prix (€)", type: "tel"}), 
+					React.createElement(MaterializeInput, {className: "col s6", name: "count", label: "Nombre", type: "tel"}), 
+					React.createElement(MaterializeInput, {className: "col s6", name: "price", label: "Prix (€)", type: "tel"}), 
 					React.createElement("button", {className: "col s12 waves-effect waves-light btn-large green"}, "Ajouter")
 				)
 			)
@@ -220,14 +220,15 @@ var MaterializeInput = React.createClass({displayName: 'MaterializeInput',
 	propsType: {
 		placeholder: React.PropTypes.string,
 		type: React.PropTypes.string,
-		label: React.PropTypes.string
+		label: React.PropTypes.string,
+		name: React.PropTypes.string,
 	},
 
 	render: function () {
 		return (
 			React.createElement("div", {className: (this.props.className || '') + " input-field"}, 
-				React.createElement("input", {id: this.props.label, placeholder: this.props.placeholder, type: this.props.type, className: "validate"}, 
-					React.createElement("label", {htmlFor: this.props.label}, this.props.label)
+				React.createElement("input", {id: this.props.name, placeholder: this.props.placeholder, type: this.props.type, className: "validate"}, 
+					React.createElement("label", {htmlFor: this.props.name}, this.props.label)
 				)
 			)
 		)
